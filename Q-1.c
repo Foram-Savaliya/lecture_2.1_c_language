61e9504274b795f7e6ed1f089449b9f4dbd96e37
Foram Savaliya <3383foramsavaliya@gmail.com>
2023-12-06 11:02:30 +0530

    first commit

---
+++ Q-1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+	int a=12;
+	int b=6;
+	
+	printf("addition of %d and %d is %d\n",a,b,a+b);
+	printf("subtraction of %d and %d is %d\n",a,b,a-b);
+	printf("Multiplication of %d and %d is %d\n",a,b,a*b);
+	printf("Division of %d and %d is %d\n",a,b,a/b);
+	printf("Modulo of %d and %d is %d\n\n",a,b,a%b);
+	
+	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d - %d = %d\n",a,b,a-b);
+	printf("%d * %d = %d\n",a,b,a*b);
+	printf("%d / %d = %d\n",a,b,a/b);
+	printf("%d mod %d = %d\n",a,b,a%b);
+}
